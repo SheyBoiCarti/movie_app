8ebcacbfa4dfaf038b4db091db1a1eb86441102c
SheyBoiCarti <s225824817@mandela.ac.za>
2025-12-02 12:41:50 +0200

    routing example

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-   <View className='flex-1 justify-center items-center'>
-    <Text className="text-5xl text-dark-100 font-center" > Welcome to my movie app</Text>
-
-    <Link href='/onboarding'> Onboarding</Link>
-    </View>
+    
+      <View className='flex-1 justify-center items-center'> 
+      <Text className="text-5xl text-dark-100 font-center" > 
+        Welcome! </Text>
+        
+      <Link href="/onboarding">Go to onboarding</Link>
+      <Link href={{pathname: "/movie/[id]", params: { id: "avengers" }  }}>avengers movie </Link>
+      </View>
   );
 }
